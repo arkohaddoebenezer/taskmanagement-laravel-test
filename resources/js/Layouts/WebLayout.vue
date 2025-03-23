413fae44510d38aef7bbaf8553b4b320b1da79ff
arkohaddoebenezer <arkohaddoebenezer21@gmail.com>
2025-03-23 10:12:30 +0000

    initial setup

---
+++ resources/js/Layouts/WebLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <div v-if="$slots.header" class="px-3 mx-auto">
+            <slot name="header" />
+        </div>
+    </header>
+    <div class="container">
+        <slot />
+        <cart-count />
+    </div>
+</template>
+<script>
+</script>
