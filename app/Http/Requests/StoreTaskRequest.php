f0e0f02c36436e8a257611dd790f49d5753a2adf
arkohaddoebenezer <arkohaddoebenezer21@gmail.com>
2025-03-24 15:09:42 +0000

    feat: Add Task management functionality with requests, model, policy, migration, factory, and seeder

---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Task;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function prepareForValidation()
+    {
+        $this->merge([
+            'name' => ucfirst(strtolower($this->name)),
+            'priority' => $this->priority ?? (Task::count() + 1),
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        
+        return [
+            'name' => "required|min:2|max:255",
+            'priority' => "required|integer|min:1",
+            'project_id' => "nullable|exists:projects,id",
+        ];
+    }
+}
